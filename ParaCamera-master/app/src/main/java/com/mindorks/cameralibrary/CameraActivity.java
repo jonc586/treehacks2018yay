5921f1e4b747f1350e47b2e8f015279621fa0cf2
ClearSpear <zengcs@stanford.edu>
2018-02-17 19:18:30 -0800

    app has restart button now

--- ParaCamera-master/app/src/main/java/com/mindorks/cameralibrary/CameraActivity.java
+++ ParaCamera-master/app/src/main/java/com/mindorks/cameralibrary/CameraActivity.java
@@ -1,7 +1,14 @@
 package com.mindorks.cameralibrary;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+
+        final Button button = (Button) findViewById(R.id.button_restart);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Toast toast = Toast.makeText(getApplicationContext(), "Refreshing", Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
     }
 }
